b5c4abf384e343a118c9f4ed259ebfb9dd263247
Aqib2110 <muhammadaqib2110@gmail.com>
2026-03-15 17:28:55 +0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+import express from 'express'
+dotenv.config({
+    path:"./secrets/.env"
+})
+const app = express();
+
+app.get("/api",(req,res)=>{
+    return res.json({
+        port:process.env.PORT,
+        databaseUrl:process.env.DATABASE_URL,
+    })
+})
+app.listen(process.env.PORT,()=>{
+    console.log("app is listening on PORT",process.env.PORT)
+});
